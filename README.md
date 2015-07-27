7319279bca79b0d9e2fe14b0d199dbf95c424191
Tom Batchelor <tbatchelor@xebialabs.com>
2015-07-27 16:12:02 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# xlr-teamcity-plugin
-Plugin to trigger builds in TeamCity
+# Preface #
+
+This document describes the xlr-teamcity-plugin.
+
+See the **XL Release Reference Manual** for background information on XL Release and release concepts.
+
+# Overview #
+
+A Plugin to trigger builds in TeamCity. Currently a very simple implementation which only supports triggering a build using the TeamCity buildConfID and returns the build number.
+
+## Installation ##
+
+Place the latest released version under the `plugins` dir.
+
+## Types ##
+
++ Build 
+  * `server` - TeamCity server definition (see below)
+  * `username` - Used to override the TeamCity username - optional 
+  * `password` - Used to override the TeamCity password - optional
+  * `buildConfID` - The buildConfID for the TeamCity build job, usually found in the settings screen for the build job.
+  * `buildNumber` - Output field where the build number will be set on successful completion
++ Server - In the XL Release Configuration screen
+  * `name` - Friendly name for this TeamCity server
+  * `URL` - URL to the TeamCity server, include port here
+  * `username` - Default username for TeamCity
+  * `password` - Password for the default user
+  * `proxyHost` - Proxy host if required
+  * `proxyPort` - Proxy port if required
---
+++ src/main/resources/teamcity/Build.py
@@ -0,0 +1,48 @@
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#
+
+import sys, time, ast, re
+from xml.etree import ElementTree as ET
+from httputil.HttpRequest import HttpRequest
+
+poll_interval = 5
+
+httpRequest = HttpRequest(teamcityServer, username, password)
+
+queueBuildURL = 'httpAuth/app/rest/buildQueue'
+buildBody = '<build><buildType id="' + buildID + '"/></build>'
+
+queue_response = httpRequest.post(queueBuildURL, buildBody, contentType='application/xml')
+
+if queue_response.isSuccessful():
+    # polls until the job completes
+    root = ET.fromstring(queue_response.getResponse())
+    runID = root.attrib['id']
+    statusURL = 'httpAuth/app/rest/buildQueue/taskId:' + runID
+    while True:
+        time.sleep(poll_interval)
+        status_response = httpRequest.get(statusURL, contentType='application/xml')
+        if status_response.isSuccessful():
+            root = ET.fromstring(status_response.getResponse())
+            if root.attrib['state'] == 'finished':
+                # Have a build completed
+                if root.attrib['status'] == 'SUCCESS':
+                    buildNumber = root.attrib['number']
+                    print('Build successful with build numner: ' + buildNumber)
+                    sys.exit(0)
+                else:
+                    print('Build failed, run ID: ' + runID)
+                    print(status_response.getResponse())
+                    sys.exit(1)
+            else:
+                print('Waiting for buildID: ' + buildID + ' to start/finish')
+                continue
+        else:
+            print('Status request failed')
+            sys.exit(1)
+else:
+    print('Queuing failed for buildID: ' + buildID)
+
