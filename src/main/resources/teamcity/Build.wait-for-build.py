25a18fbc17f9acbf11af2919ab56f184c71ed36f
ndebuhr <ndebuhr@protonmail.com>
2019-03-09 21:19:25 -0500

    Further refactoring and refining of build task control flow.  As a part of this, set status line updates and provide a couple new properties (both optional) - Poll Interval input and Task ID output

--- src/main/resources/teamcity/Build.wait-for-build.py
+++ src/main/resources/teamcity/Build.wait-for-build.py
@@ -7,35 +7,40 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-
+import sys
 import com.xhaus.jyson.JysonCodec as json
 from xlrelease.HttpRequest import HttpRequest
 from teamcity.util import error
 
 request = HttpRequest(teamcityServer, username, password)
+statusURL = 'httpAuth/app/rest/buildQueue/taskId:' + str(taskID)
+status_response = request.get(statusURL, contentType='application/json')
 
-queueBuildURL = 'httpAuth/app/rest/buildQueue'
+if not status_response.isSuccessful():
+    error(
+        'Request failed - couldn\'t poll for status of build from task ID ' + taskID,
+        status_response
+    )
 
-buildBody = {
-    'buildType': {
-        'id': buildID
-    },
-}
+resp = json.loads(status_response.response)
 
-if len(buildProperties) > 0:
-    buildBody['properties'] = {"property": []}
-    for prop in buildProperties.keys():
-        buildBody["properties"]["property"].append(
-            {
-                "name": prop,
-                "value": buildProperties[prop]
-            }
+if resp['state'] == 'finished':
+    buildNumber = resp['number']
+    # Have a build completed
+    if resp['status'] == 'SUCCESS':
+        print('Build successful - build ' + str(buildNumber))
+        sys.exit(0)
+    else:
+        print('Build failed - build ' + str(buildNumber))
+        print(status_response.getResponse())
+        sys.exit(1)
+else:
+    if 'waitReason' in resp.keys():
+        task.setStatusLine(
+            'Build is ' + resp['state'] + \
+            '.  ' + resp['waitReason'] + '...'
         )
-
-queue_response = request.post(queueBuildURL, json.dumps(buildBody), contentType='application/json')
-
-if not queue_response.isSuccessful():
-    error('Queuing failed for build configuration ID ' + buildID, queue_response)
-resp = json.loads(queue_response.response)
-taskID = str(resp['id'])
-task.schedule('teamcity/Build.wait-for-build.py')
+        task.schedule("teamcity/Build.wait-for-build.py", pollInterval)
+    else:
+        task.setStatusLine('Build is ' + resp['state'] + '...')
+        task.schedule("teamcity/Build.wait-for-build.py", pollInterval)
