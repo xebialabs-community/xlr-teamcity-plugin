c8c4d6e0b4e6f8fa0c8b427d2a77ab7afa9f190f
Joris De Winne <joris.dewinne@gmail.com>
2019-04-10 07:14:32 -0700

    Taking into account an unknown status to be shown as red

--- src/main/resources/teamcity/BuildConfigurationsStatusTile.py
+++ src/main/resources/teamcity/BuildConfigurationsStatusTile.py
@@ -23,7 +23,7 @@ def increment_status_count(status):
     if status not in status_options:
         status_options.append(status)
     if not any(d['name'] == status for d in status_count):
-        status_count.append({'name':status,'value':0, 'itemStyle':{'color':status_colors[status]}})
+        status_count.append({'name':status,'value':0, 'itemStyle':{'color':status_colors[status] if status in status_colors else 'red'}})
     d = next(d for i,d in enumerate(status_count) if d['name'] == status)
     d['value'] += 1
     logger.info("status_count [%s]" % status_count)
